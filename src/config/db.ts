320223d222613217d75070a6daf37b8fd966a11d
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-06-27 00:26:02 +0600

    added the status

--- src/config/db.ts
+++ src/config/db.ts
@@ -37,8 +37,8 @@ const createUsersTable = `
 
      reporter_id INT NOT NULL,
     
-//     created_at TIMESTAMPTZ DEFAULT NOW(),
-//     updated_at TIMESTAMPTZ DEFAULT NOW(),
+     created_at TIMESTAMPTZ DEFAULT NOW(),
+     updated_at TIMESTAMPTZ DEFAULT NOW(),
 
 //     CONSTRAINT fk_reporter
 //       FOREIGN KEY (reporter_id)
