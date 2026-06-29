d0a9111f7fd88050f9044b58b29fd19846e182f2
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-06-30 00:19:36 +0600

    added the next

--- src/middleware/error.middleware.ts
+++ src/middleware/error.middleware.ts
@@ -6,7 +6,7 @@
    _req: Request,
    res: Response,
    _next: NextFunction,
-// ) => {
+ ) => {
 //   const message = err instanceof Error ? err.message : "Internal Server Error";
 
 //   res.status(500).json({
