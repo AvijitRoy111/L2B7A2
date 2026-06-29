f8be3a6d53cd36765873b7c46738dabac219428b
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-06-29 20:15:15 +0600

    added the response

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -5,8 +5,8 @@
  import sendResponse from "../utility/sendResponse";
  import type { NextFunction, Request, Response } from "express";
 
-// const auth = async (req: Request, res: Response, next: NextFunction) => {
-//   try {
+ const auth = async (req: Request, res: Response, next: NextFunction) => {
+   try {
 //     const token = req.headers.authorization;
 
 //     if (!token) {
