75a7dd75da4ebc85c8126e8748d1f0abbf0dfb94
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-07-02 00:05:38 +0600

    added the data

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -7,7 +7,7 @@
    const { email, password } = payload;
 
    const userdata = await pool.query(
-//     `
+     `
 //            SELECT * FROM users WHERE email=$1
 //            `,
 //     [email],
