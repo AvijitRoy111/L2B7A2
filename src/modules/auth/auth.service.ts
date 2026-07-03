93f19b78d5f37527dbf9b07836880782198ae21b
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-07-03 11:22:12 +0600

    added the handle password

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -70,7 +70,7 @@ const signUpUserIntoDB = async (payload: TSignUpUser) => {
      `
      INSERT INTO users (name, email, password, role)
      VALUES ($1, $2, $3, $4)
-//     RETURNING id, name, email, role, created_at, updated_at
+     RETURNING id, name, email, role, created_at, updated_at
 //   `,
 //     [name, email, hashedPassword, userRole],
 //   );
