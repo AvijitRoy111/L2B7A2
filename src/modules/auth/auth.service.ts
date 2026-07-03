7a00c562b88f369856fecf8db9d53d97e34a3d3b
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-07-03 11:16:53 +0600

    added the email

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -36,7 +36,7 @@
    const returnUser = {
      id: user.id,
      name: user.name,
-//     email: user.email,
+     email: user.email,
 //     role: user.role,
 //     created_at: user.created_at,
 //     updated_at: user.updated_at,
