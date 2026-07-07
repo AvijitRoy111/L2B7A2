bcb0e538b04be212a1ab706a13d4c6c756830a16
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-07-07 12:56:46 +0600

    added the code

--- src/modules/issues/issue.controller.ts
+++ src/modules/issues/issue.controller.ts
@@ -58,7 +58,7 @@
        statusCode,
        success: false,
        message: error instanceof Error ? error.message : "Internal Server Error",
-//       errors: error,
+       errors: error,
 //     });
 //   }
 // };
