dad40f967cbe3bb2bb51cbb7d2fc1242f6495d02
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-07-10 10:28:13 +0600

    added the issue

--- src/modules/issues/issue.route.ts
+++ src/modules/issues/issue.route.ts
@@ -1,9 +1,9 @@
  import { Router } from "express";
  import auth from "../../middleware/auth.middleware";
-// import { issueController } from "./issue.controller";
-// import role from "../../middleware/role.middleware";
+ import { issueController } from "./issue.controller";
+ import role from "../../middleware/role.middleware";
 
-// const router = Router();
+ const router = Router();
 
 
 
