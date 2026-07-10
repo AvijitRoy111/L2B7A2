d17d88581e722bc1c19c25b4cbc30fefa860acf1
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-07-10 10:26:45 +0600

    added the massage

--- src/modules/issues/issue.controller.ts
+++ src/modules/issues/issue.controller.ts
@@ -188,8 +188,8 @@
        statusCode,
        success: false,
        message: error instanceof Error ? error.message : "Internal Server Error",
-//       errors: error,
-//     });
+       errors: error,
+     });
 //   }
 // };
 
