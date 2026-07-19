10ea8b3a20d90103ac726a10268759c716892f6a
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-07-19 19:34:13 +0600

    added the status

--- src/utility/sendResponse.ts
+++ src/utility/sendResponse.ts
@@ -1,8 +1,8 @@
  import type { Response } from "express";
 
  interface TResponse<T> {
-//   statusCode: number;
-//   success: boolean;
+   statusCode: number;
+   success: boolean;
 //   message: string;
 //   data?: T;
 //   errors?: unknown;
