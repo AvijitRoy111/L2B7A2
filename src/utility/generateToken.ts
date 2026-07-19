69f519f53770e8148a028aaf40d2bea37661487d
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-07-19 19:31:21 +0600

    added the jwt

--- src/utility/generateToken.ts
+++ src/utility/generateToken.ts
@@ -1,7 +1,7 @@
  import jwt, { type JwtPayload } from "jsonwebtoken";
  import config from "../config";
 
-// const generateToken = (payload: JwtPayload) => {
+ const generateToken = (payload: JwtPayload) => {
 //   const token = jwt.sign(
 //     payload as JwtPayload,
 //     config.ACCESS_SECRET as string,
