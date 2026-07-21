46cc2395557eb8b671e008f7d50fee4acd4d95bf
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-07-21 19:29:16 +0600

    added the data

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,7 @@
  app.use(cookie());
  app.use(express.json());
  app.use(express.text());
-// app.use(express.urlencoded({ extended: true }));
+ app.use(express.urlencoded({ extended: true }));
 
 // app.use(
 //   cors({
