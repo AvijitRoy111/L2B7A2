b3aac1fedd61c28599e1d5ddd4f9621cc76c6abe
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-07-24 20:50:22 +0700

    added the update

--- src/server.ts
+++ src/server.ts
@@ -24,7 +24,7 @@
  })();
 
  const main = async () => {
-//   await initDB();
+   await initDB();
 //   app.listen(config.PORT, () => {
 //     console.log(`L2B7A2 app listening on port ${config.PORT}`);
 //   });
