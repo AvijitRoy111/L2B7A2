2d995f9c166e9a6361d3d6cef1ad388eb2d85c88
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-08-13 18:23:36 +0600

    added the type

--- src/config/db.ts
+++ src/config/db.ts
@@ -32,7 +32,7 @@
       type VARCHAR(20) NOT NULL
         CHECK (type IN ('bug', 'feature_request')),
 
-//      status VARCHAR(20) NOT NULL DEFAULT 'open'
+      status VARCHAR(20) NOT NULL DEFAULT 'open'
 //        CHECK (status IN ('open', 'in_progress', 'resolved')),
 
 //      reporter_id INT NOT NULL,
