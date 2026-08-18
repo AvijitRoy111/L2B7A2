b12194f449c0e3161e5ddf4e20c834ff1d6565e9
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-08-18 18:07:58 +0600

    added the create user

--- src/config/db.ts
+++ src/config/db.ts
@@ -38,7 +38,7 @@
       reporter_id INT NOT NULL,
     
       created_at TIMESTAMPTZ DEFAULT NOW(),
-//      updated_at TIMESTAMPTZ DEFAULT NOW(),
+      updated_at TIMESTAMPTZ DEFAULT NOW(),
 
 //      CONSTRAINT fk_reporter
 //        FOREIGN KEY (reporter_id)
