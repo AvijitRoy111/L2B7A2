ffbc89b2ebaa72dae9242abb52f79b736cd21680
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-08-18 18:13:03 +0600

    added the dot

--- src/config/index.ts
+++ src/config/index.ts
@@ -6,7 +6,7 @@
  });
 
   const config = {
-//    PORT: process.env.PORT,
+    PORT: process.env.PORT,
 //    DATABASE_URL: process.env.DATABASE_URL,
 //    ACCESS_SECRET: process.env.ACCESS_SECRET,
 //    REFRESH_SECRET: process.env.REFRESH_SECRET,
