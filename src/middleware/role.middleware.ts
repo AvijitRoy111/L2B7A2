0f914f8b6775d49a7c1a865427d3608096e86316
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-08-22 17:06:16 +0600

    added the includes

--- src/middleware/role.middleware.ts
+++ src/middleware/role.middleware.ts
@@ -13,7 +13,7 @@
      }
 
      if (!roles.includes(req.user.role)) {
-      //  return sendResponse(res, {
+       return sendResponse(res, {
         //  statusCode: 403,
         //  success: false,
         //  message: "Forbidden access!!",
