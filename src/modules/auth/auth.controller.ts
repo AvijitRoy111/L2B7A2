8fa1a63f37890b5d397a7f336c576c7cc284d89a
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-08-24 11:13:49 +0600

    added the res

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -21,7 +21,7 @@
      sendResponse(res, {
        statusCode,
        success: false,
-      //  message: error instanceof Error ? error.message : "Internal Server Error",
+       message: error instanceof Error ? error.message : "Internal Server Error",
       //  errors: error,
     //  });
   //  }
