ed1c635ab1e955e124f284a2a6b6fe443912a7e9
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-08-25 13:11:52 +0600

    added the signup

--- src/modules/auth/auth.route.ts
+++ src/modules/auth/auth.route.ts
@@ -1,9 +1,9 @@
  import { Router } from "express";
  import { authController } from "./auth.controller";
 
-//  const router = Router();
+ const router = Router();
 
-//  router.post("/signup", authController.signUpUser);
+ router.post("/signup", authController.signUpUser);
 //  router.post("/login", authController.loginUser);
 
 //  export const authRoute = router;
