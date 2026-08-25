b4dba2bb82a423cea2ae27e07b5c4ef0da31d661
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-08-25 13:06:12 +0600

    added the error

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -43,7 +43,7 @@
          error instanceof Error && error.message === "User already exists"
            ? 409
            : error instanceof Error &&
-              //  error.message === "Name, email and password are required!"
+               error.message === "Name, email and password are required!"
             //  ? 400
             //  : 500,
       //  success: false,
