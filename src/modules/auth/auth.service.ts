9b7d751ac9963610747fac0428979026b8f58f99
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-08-30 18:07:39 +0600

    added the new

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -62,7 +62,7 @@ const signUpUserIntoDB = async (payload: TSignUpUser) => {
 
    if (existingUser.rows.length > 0) {
      throw new Error("User already exists");
-  //  }
+   }
 
   //  const hashedPassword = await bcrypt.hash(password, 10);
 
