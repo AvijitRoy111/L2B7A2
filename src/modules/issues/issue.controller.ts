aa0ac46461b1619d7b1f026a68f2b1cb04fc7e71
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-09-04 16:15:13 +0600

    added the code

--- src/modules/issues/issue.controller.ts
+++ src/modules/issues/issue.controller.ts
@@ -37,7 +37,7 @@
     //  const result = await issueService.getSingleIssueFromDB(id as string);
 
     //  if (!result) {
-       return sendResponse(res, {
+      //  return sendResponse(res, {
          statusCode: 404,
          success: false,
          message: "Issue not found",
