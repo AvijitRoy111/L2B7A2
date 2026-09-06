4321d99d1fffc89f27f14fa2fbe8977709a098df
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-09-06 17:28:03 +0600

    added the try

--- src/modules/issues/issue.controller.ts
+++ src/modules/issues/issue.controller.ts
@@ -66,7 +66,7 @@
 //  const createIssue = async (req: Request, res: Response) => {
   //  try {
     //  if (!req.user?.id) {
-       return sendResponse(res, {
+      //  return sendResponse(res, {
          statusCode: 401,
          success: false,
          message: "Unauthorized access!!",
