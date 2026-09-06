85ae64d7a0a4d6f1253e2976f83e8c4c55f09057
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-09-06 17:26:25 +0600

    added the error

--- src/modules/issues/issue.controller.ts
+++ src/modules/issues/issue.controller.ts
@@ -58,8 +58,8 @@
       //  statusCode,
       //  success: false,
       //  message: error instanceof Error ? error.message : "Internal Server Error",
-       errors: error,
-     });
+      //  errors: error,
+    //  });
    }
  };
 
