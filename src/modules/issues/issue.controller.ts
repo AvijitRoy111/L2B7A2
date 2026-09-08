58c79835c4a6a0f70f1c5928eaa85d05b7ca16e0
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-09-08 19:14:53 +0600

    added the responce

--- src/modules/issues/issue.controller.ts
+++ src/modules/issues/issue.controller.ts
@@ -95,7 +95,7 @@
 
     //  sendResponse(res, {
       //  statusCode,
-       success: false,
+      //  success: false,
        message: error instanceof Error ? error.message : "Internal Server Error",
       errors: error,
      });
