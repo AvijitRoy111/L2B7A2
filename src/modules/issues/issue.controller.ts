db2bae28b658bd55f9043265648e8f14ed061cdf
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-09-08 19:13:47 +0600

    added the tittle

--- src/modules/issues/issue.controller.ts
+++ src/modules/issues/issue.controller.ts
@@ -88,7 +88,7 @@
       //  error instanceof Error &&
       //  (error.message === "Title, description and type are required" ||
         //  error.message === "Title must be less than or equal to 150 characters" ||
-         error.message === "Description must be at least 20 characters" ||
+        //  error.message === "Description must be at least 20 characters" ||
          error.message === "Invalid issue type")
          ? 400
          : 500;
