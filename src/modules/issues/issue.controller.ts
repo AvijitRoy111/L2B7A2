87d21e92a5d8a1866c8daea27665980d3368a4d5
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-09-08 19:14:02 +0600

    added the massage

--- src/modules/issues/issue.controller.ts
+++ src/modules/issues/issue.controller.ts
@@ -89,7 +89,7 @@
       //  (error.message === "Title, description and type are required" ||
         //  error.message === "Title must be less than or equal to 150 characters" ||
         //  error.message === "Description must be at least 20 characters" ||
-         error.message === "Invalid issue type")
+        //  error.message === "Invalid issue type")
          ? 400
          : 500;
 
