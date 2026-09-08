2650643f601687d1191183cbddc614a4178140a8
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-09-08 19:16:29 +0600

    added the result

--- src/modules/issues/issue.controller.ts
+++ src/modules/issues/issue.controller.ts
@@ -105,7 +105,7 @@
 //  const updateIssue = async (req: Request, res: Response) => {
   //  const { id } = req.params;
   //  try {
-    const result = await issueService.updateIssueIntoDB(
+    // const result = await issueService.updateIssueIntoDB(
        id as string,
        req.body,
        req.user as JwtPayload,
