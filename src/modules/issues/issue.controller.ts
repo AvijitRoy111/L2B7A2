03c1f5323a802a13ed1a351e6f5bf04795a55ccb
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-09-11 16:49:34 +0600

    added the status

--- src/modules/issues/issue.controller.ts
+++ src/modules/issues/issue.controller.ts
@@ -132,7 +132,7 @@
       //  if (error.message === "Issue not found") {
         //  statusCode = 404;
       //  } else if (error.message === "Invalid issue id") {
-         statusCode = 400;
+        //  statusCode = 400;
        } else if (
          error.message === "Forbidden access" ||
          error.message === "Contributor cannot update issue status"
