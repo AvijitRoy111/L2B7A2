7155c8cdbf200d0b148e747873f70b7d3d37ee4e
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-09-11 16:53:37 +0600

    added the title

--- src/modules/issues/issue.controller.ts
+++ src/modules/issues/issue.controller.ts
@@ -147,7 +147,7 @@
         //  error.message === "Invalid issue status" ||
         //  error.message ===
           //  "Title must be less than or equal to 150 characters" ||
-         error.message === "Description must be at least 20 characters"
+        //  error.message === "Description must be at least 20 characters"
        ) {
          statusCode = 400;
        }
