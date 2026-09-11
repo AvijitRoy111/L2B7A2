ab40f43b73883fbbbabbec17b8241eb2fb6c5a96
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-09-11 16:49:05 +0600

    added the code

--- src/modules/issues/issue.controller.ts
+++ src/modules/issues/issue.controller.ts
@@ -130,7 +130,7 @@
 
     //  if (error instanceof Error) {
       //  if (error.message === "Issue not found") {
-         statusCode = 404;
+        //  statusCode = 404;
        } else if (error.message === "Invalid issue id") {
          statusCode = 400;
        } else if (
