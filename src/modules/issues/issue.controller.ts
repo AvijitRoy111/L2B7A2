e6774dea174de5ed4d65650fd4bddbdb0cf1d63e
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-09-17 19:28:06 +0600

    added the issue

--- src/modules/issues/issue.controller.ts
+++ src/modules/issues/issue.controller.ts
@@ -64,7 +64,7 @@
  };
 
  const createIssue = async (req: Request, res: Response) => {
-  //  try {
+   try {
     //  if (!req.user?.id) {
       //  return sendResponse(res, {
         //  statusCode: 401,
