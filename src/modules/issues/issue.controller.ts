9f57c9154c7377c5a075ba898a29dcb56ef6e4a6
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-09-18 16:45:21 +0600

    added the type

--- src/modules/issues/issue.controller.ts
+++ src/modules/issues/issue.controller.ts
@@ -90,7 +90,7 @@
          error.message === "Title must be less than or equal to 150 characters" ||
          error.message === "Description must be at least 20 characters" ||
          error.message === "Invalid issue type")
-        //  ? 400
+         ? 400
         //  : 500;
 
     //  sendResponse(res, {
