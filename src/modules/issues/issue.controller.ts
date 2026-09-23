6e4ab89d7f5be34f8a2385686d729f6fc18fce0a
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-09-23 18:52:48 +0600

    added the code

--- src/modules/issues/issue.controller.ts
+++ src/modules/issues/issue.controller.ts
@@ -186,7 +186,7 @@
 
      sendResponse(res, {
        statusCode,
-      //  success: false,
+       success: false,
       //  message: error instanceof Error ? error.message : "Internal Server Error",
       //  errors: error,
     //  });
