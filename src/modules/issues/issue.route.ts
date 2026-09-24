5122e596177fccf464afdd0a49e223cf80283e8e
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-09-24 19:30:24 +0600

    added the patch

--- src/modules/issues/issue.route.ts
+++ src/modules/issues/issue.route.ts
@@ -11,7 +11,7 @@
 
 //  router.post("/", auth, issueController.createIssue);
 
- router.patch("/:id", auth, issueController.updateIssue);
+//  router.patch("/:id", auth, issueController.updateIssue);
 
  router.delete("/:id", auth, role("maintainer"), issueController.deleteIssue);
 
